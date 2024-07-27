955ddc01aa02dd02878bf90bb839ec7e56a8c0aa
madenon <brcgr230@gmail.com>
2024-07-27 18:13:38 +0100

    creation des routes et le test

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
-dotenv.config()
+import userRouter  from "./routes/user.route.js"
 
+dotenv.config()
 mongoose.connect(process.env.MONGO_URI).then(() =>{
     console.log('Connexion  a la  base ok')
 }).catch((err)=>{
@@ -11,7 +12,13 @@ mongoose.connect(process.env.MONGO_URI).then(() =>{
 
 const app = express()
 
+app.get('/', (req, res) =>{
+    res.json({message:"Hello"})
+})
 
-app.listen(3000, () =>{
+app.use("/api",  userRouter)
+
+
+app.listen(5000, () =>{
     console.log(`Server ok au port: http://localhost:${process.env.PORT}`)
 })
