f2d4fd945a5a4db05373b9c90c1dc54ea8503462
madenon <brcgr230@gmail.com>
2024-07-27 17:46:00 +0100

    La connexion a la base de donnee

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express"
-const app =express() 
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI).then(() =>{
+    console.log('Connexion ok')
+}).catch((err)=>{
+    console.log(Error)
+});
+
+const app = express()
+
 
 app.listen(3000, () =>{
-    console.log(`Server ok ok `)
+    console.log(`Server ok`)
 })
