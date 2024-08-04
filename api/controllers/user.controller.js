72b8b169b6c68c6aec0a2810d39d8a592b6a72d9
madenon <brcgr230@gmail.com>
2024-08-04 14:31:13 +0100

    La suppression du compte  du api et du frontend

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -4,35 +4,49 @@ import { errorHandler } from "../utils/error.js";
 
 
 
-export const  test = (req, res) =>{
+export const test = (req, res) => {
     res.json({
-        message:"Test ok"
+        message: "Test ok"
     })
 
 };
 
 
-export const updateUser  =  async(req, res ,next) =>{
-    if(req.user.id !== req.params.id ) return next(errorHandler(401, "vous ne pouvez pas mettre à jour le compte"));
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "vous ne pouvez pas mettre à jour le compte"));
 
     try {
-        if(req.body.password){
-            req.body.password =   bcryptjs.hashSync(req.body.password, 10)
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
         }
         const updateUser = await User.findByIdAndUpdate(req.params.id, {
-            $set:{
-                username:req.body.username,
-                email:req.body.email,
-                password:req.body.password,
-                avatar:req.body.avatar
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
             }
-        }, {new:true})
-        const {password, ...rest} = updateUser._doc;
+        }, { new: true })
+        const { password, ...rest } = updateUser._doc;
         res.status(200).json(rest)
-        
+
     } catch (error) {
         next(error)
-        
+
     }
 
+}
+
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "Cet compte ne peut-etre supprimer"))
+
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie('access_token');
+        res.status(200).json("Utilisateur supprimer avec succès")
+
+    } catch (error) {
+        next(error)
+
+    }
 }
