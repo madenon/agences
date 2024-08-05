86f772ee354def43b66798b2b3006b6bcbaf0a8a
madenon <brcgr230@gmail.com>
2024-08-05 23:32:20 +0100

    backend de  mise a jour

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -13,6 +13,33 @@ export const createListing =  async(req, res, next) =>{
     }
 }
 
+export const editListing = async(req, res, next) =>{
+
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404, "L'annonce introuvable"));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, "Vous puvez seuelemt mettre a jour vos annonces "))
+    }
+    try {
+        
+        const  updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+        res.status(200).json(updatedListing)
+
+    } catch (error) {
+        next(error)
+        
+    }
+}
+
+
+
 export const  deleteListing = async(req, res, next)=>{
     const listing = await Listing.findById(req.params.id)
     if(!listing){
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, editListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing)
+router.post("/edit/:id", verifyToken, editListing)
 router.delete("/delete/:id", verifyToken, deleteListing)
 
 
