30180c0aee2e1e244ba97ef9171ca91bf2360863
madenon <brcgr230@gmail.com>
2024-08-05 14:59:39 +0100

    dans la creation et gestion des erreurs de CreateListing

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -5,7 +5,7 @@ const listingSchema = new mongoose.Schema({
     address:{type:String, required:true},
     regularPrice:{type:Number, required:true},
     discountPrice:{type:Number, required:true},
-    bathRooms:{type:Number, required:true},
+    bathrooms:{type:Number, required:true},
     bedrooms:{type:Number, required:true},
     furnished:{type:Boolean, required:true},
     parking:{type:Boolean, required:true},
