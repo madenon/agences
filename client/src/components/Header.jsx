470971d4798233ae7c57238c6f1decfa02f8f7b7
madenon <brcgr230@gmail.com>
2024-08-08 19:44:25 +0100

    revu du lien de connexion

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,12 +30,12 @@ useEffect(() =>{
     <header className='bg-slate-200 shadow-md'>
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className='font-bold text-sm sm:text-xl flex  flex-wrap'>
-            <span className='text-slate-500'>Client</span>
+          <h1 className='font-bold text-sm sm:text-xl flex  flex-wrap '>
+            <span className='text-slate-500'>Agence</span>
             <span className='text-slate-700'>Agence</span>
           </h1>
         </Link>
-        <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form onSubmit={handleSubmit} className='bg-slate-100 p-2 rounded-lg flex items-center'>
           <input type="text" 
           placeholder='Recherche ...' 
           className='bg-transparent focus:outline-none w-24 sm:w-64 max-lg:w-20' 
@@ -50,15 +50,15 @@ useEffect(() =>{
           {/* A supprimer si le comportement devient bizzare en media scremm  max-lg: */}
 
         </form>
-        <ul className='flex gap-2'>
-          <Link to="/"><li className='hidden sm:inline text-slate-700 hover:underline'>Acueil</li>
+        <ul className='flex gap-1'>
+          <Link to="/"><li className='hidden sm:inline  hover:underline text-purple-700'>acueil</li>
           </Link>
-          <Link to='/about'><li className='sm:inline text-slate-700 hover:underline overflow-hidden'>à propos</li>
+          <Link to='/about'><li className='sm:inline text-purple-700 hover:underline overflow-hidden'>àpropos</li>
           </Link>
           <Link to="/profile">
             {currentUser ?
-              <img className='rounded-full h-7 w-7 object-cover' src= {currentUser.avatar} alt="profile" /> :
-              <li className='text-slate-700 hover:underline bg-orange-500 w-32 h-7 rounded-[20px] border-r-2 text-center uppercase'>Se connecter</li>
+              <img className='rounded-full h-3 w-3 object-cover m-2' src= {currentUser.avatar} alt="profile" /> :
+              <li className='hover:underline text-center text-purple-800'>connexion</li>
 
 
             }
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -15,3 +15,22 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+
+
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "estate-mern-f7da7.firebaseapp.com",
+//   projectId: "estate-mern-f7da7",
+//   storageBucket: "estate-mern-f7da7.appspot.com",
+//   messagingSenderId: "702229633810",
+//   appId: "1:702229633810:web:c5bc76341b2fc3f40231cb"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
