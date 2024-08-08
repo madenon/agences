40893e1c88b471d052293b4b2e5891b38fe6cf3c
madenon <brcgr230@gmail.com>
2024-08-08 15:05:34 +0100

    ok

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -31,7 +31,7 @@ useEffect(() =>{
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className='font-bold text-sm sm:text-xl flex  flex-wrap'>
-            <span className='text-slate-500'>Nabini</span>
+            <span className='text-slate-500'>Client</span>
             <span className='text-slate-700'>Agence</span>
           </h1>
         </Link>
